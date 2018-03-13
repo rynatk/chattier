01255175f5c93e167383e5d4ea1561c58d93c9a8
Ryan Atkinson <ryan@echobind.com>
2018-03-13 10:27:37 -0400

    apollo v2 client setup

--- client/src/App.js
+++ client/src/App.js
@@ -12,26 +12,58 @@ import {
   View,
 } from 'react-native';
 
+// _____________________________
+// OLD... Apollo V1.X
+// _____________________________
+
+// import { ApolloProvider } from 'react-apollo';
 // import { createStore, combineReducers, applyMiddleware } from 'redux';
 // import { composeWithDevTools } from 'redux-devtools-extension';
 // import ApolloClient, { createNetworkInterface } from 'apollo-client';
-import ApolloClient from 'apollo-client';
-import { HttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { ApolloProvider } from 'react-apollo';
 
-const client = new ApolloClient({
-  link: new HttpLink({ uri: 'http://localhost:8080/graphql' }),
-  cache: new InMemoryCache().restore({}),
-});
+// const client = new ApolloClient({
+//   networkInterface: createNetworkInterface({ uri: 'http://localhost:8080/graphql' }),
+// });
 
 // const store = createStore(
 //   combineReducers({
 //     apollo: client.reducer(),
 //   }),
 //   {}, // initial state
-//   composeWithDevTools(applyMiddleware(client.middleware())),
+//   composeWithDevTools(
+//     applyMiddleware(client.middleware()),
+//   ),
 // );
+
+/**
+ * And below....
+ * <ApolloProvider store={store} client={client}>
+ */
+
+// _____________________________
+// NEW... Apollo V2
+// _____________________________
+
+import ApolloClient from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloLink } from 'apollo-link';
+// import gql from 'graphql-tag';
+import { ApolloProvider, graphql } from 'react-apollo';
+
+const client = new ApolloClient({
+  link: createHttpLink({ uri: 'http://localhost:8080/graphql' }),
+  cache: new InMemoryCache().restore({}), // replaces the internal redux store
+});
+
+// example logger from migration docs
+const logger = new ApolloLink((operation, forward) => {
+  console.log(operation.operationName);
+  return forward(operation).map((result) => {
+    console.log(`received result from ${operation.operationName}`);
+    return result;
+  })
+});
 
 const styles = StyleSheet.create({
   container: {
@@ -78,4 +110,4 @@ export default class App extends Component<Props> {
       </ApolloProvider>
     );
   }
-}
+};
